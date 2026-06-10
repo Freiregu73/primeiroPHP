1b4ee3ef748eaaafe237b5faa2ebb6db1fb588ad
Freiregu73 <gustavofreire191@gmail.com>
2026-06-09 22:16:25 -0300

    conectando bd

---
+++ conectar bd/pesquisar.php
@@ -0,0 +1,32 @@
+<?php 
+	$host = "localhost";
+	$user = "gusta";
+	$pwd = "123456";
+	$db = "dbloja";
+
+	$nome = $_POST['nome'];
+	$email = $_POST['email'];
+
+	$conection = mysqli_connect($host, $user, $pwd, $db);
+
+	if (mysqli_connect_errno()) {
+		echo "Erro ao conectar a base de dados" . mysqli_connect_error();
+
+	}else{
+
+		$sql = "select nome, email from tbPessoa";
+		
+		$resultado = mysqli_query($conection,$sql);
+
+		echo "<h2>Lista de pessoas<h2>";
+
+		while ($pessoa = mysqli_fetch_array($resultado)) {
+			echo $pessoa['nome'] . " - " . $pessoa['email'] . "<br><br>";
+		}
+
+		mysqli_close($conection);
+
+	}
+
+
+ ?>
